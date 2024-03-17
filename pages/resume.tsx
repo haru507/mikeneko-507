3a7449eb4883eeb7884ba8ce8bee8ab1e3c9ff35
haru507 <i.haru507@gmail.com>
2024-03-17 13:42:58 +0900

    fix:resumeに新しい項目を追加

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -8,23 +8,24 @@ function resume() {
     <div className="px-6 py-2">
       <div className="grid gap-6 md:grid-cols-2">
         <div>
-          <h5 className="my-2 text-2xl font-bold">得意なこと</h5>
+          <h5 className="my-2 text-2xl font-bold">Like and Strengths</h5>
           <div>
             <h5 className="my-2 text-xl font-bold"></h5>
-            <p className="font-semibold">フロント&サーバサイド</p>
+            <p className="font-semibold">Frontend & ServerSide</p>
             <p className="my-3">
-              React, Laravelが得意です。副業でも3件ほど携わることができました。
-              SpringBootも案件募集中です。Node.jsやGo言語などの学習も行い、携われる案件の数を増やしたいと考えています。
+              PHPとJSの経験が多く、自分自身も自信を持って設計と実装ができます！<br />
+              AWSの知識も少し持っているためその知識を活用したり、もっと学習や構築を行い、レベルアップしていきたい。
             </p>
           </div>
         </div>
         <div>
-          <h5 className="my-2 text-2xl font-bold">これからすること</h5>
+          <h5 className="my-2 text-2xl font-bold">Future challenges</h5>
           <div className="">
             <h5 className="my-2 text-xl font-bold"></h5>
-            <p className="font-semibold">モバイル&インフラ</p>
+            <p className="font-semibold">Architecture & Leader's skill</p>
             <p className="my-3">
-              FlutterやSwiftといったモバイルアプリの作成やAWS、GCPといったインフラに関する知識も必要だと感じております。今年の7月末にAWS デベロッパーアソシエイトの資格を取得しました。
+              今後の課題としてはアーキテクチャに関する知識を養い、保守性や拡張性といったところも考慮できるようにしたい。
+              また、リーダーとしては１チームを引っ張っていけるような技術力と指導力を身につけていきたい。
             </p>
           </div>
         </div>
@@ -32,7 +33,7 @@ function resume() {
 
       <div className="grid gap-6 md:grid-cols-2">
         <div>
-          <h5 className="my-3 text-2xl font-bold">言語</h5>
+          <h5 className="my-3 text-2xl font-bold">Languages</h5>
           <div className="my-2">
             {languages.map((language) => (
               <Bar data={language} key={language.name} />
@@ -40,7 +41,7 @@ function resume() {
           </div>
         </div>
         <div>
-          <h5 className="my-3 text-2xl font-bold">フレームワーク & ツール</h5>
+          <h5 className="my-3 text-2xl font-bold">Framework & Tools</h5>
           <div className="my-2">
             {tools.map((tool) => (
               <Bar data={tool} key={tool.name} />
